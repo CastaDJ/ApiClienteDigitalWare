3168417242fde8f513f5460ad515348f5c74ea1d
Juan David Castañeda <jcastanedaaguirre@gmail.com>
2025-07-15 11:06:45 -0500

    implementación service getallcliente

--- cliente/src/main/java/com/dw/cliente/services/ClienteService.java
+++ cliente/src/main/java/com/dw/cliente/services/ClienteService.java
@@ -1,5 +1,15 @@
 package com.dw.cliente.services;
 
+import java.util.List;
+
+import com.dw.cliente.entity.Cliente;
+
+
 public interface ClienteService {
 
+    List<Cliente> getAllClientes();
+    Cliente getClienteById(Integer id);
+    Cliente createCliente(Cliente cliente);
+    Cliente updateCliente(Integer id, Cliente cliente);
+    void deleteCliente(Integer id);
 }
--- cliente/src/main/java/com/dw/cliente/servicesImp/ClienteServiceImp.java
+++ cliente/src/main/java/com/dw/cliente/servicesImp/ClienteServiceImp.java
@@ -1,12 +1,51 @@
 package com.dw.cliente.servicesImp;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dw.cliente.entity.Cliente;
+import com.dw.cliente.repositories.ClienteRepository;
 import com.dw.cliente.services.ClienteService;
 
 @Service
 public class ClienteServiceImp implements ClienteService {
 
-    // Implement methods from ClienteService interface here
+    @Autowired
+    private ClienteRepository repository;
 
+    @Override
+    public List<Cliente> getAllClientes() {
+        try{
+            return repository.findAll();
+        }catch (Exception e) {
+            throw new RuntimeException("Error retrieving clients", e);
+        }
+    }
+
+    @Override
+    public Cliente getClienteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getClienteById'");
+    }
+
+    @Override
+    public Cliente createCliente(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCliente'");
+    }
+
+    @Override
+    public Cliente updateCliente(Integer id, Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateCliente'");
+    }
+
+    @Override
+    public void deleteCliente(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteCliente'");
+    }
 }
